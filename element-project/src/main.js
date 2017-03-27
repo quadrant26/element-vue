5398c087736221a826933fcd6592ae9fad77e05c
quadrant26 <quadrant26@163.com>
2017-03-27 23:37:57 +0800

    entry.js

---
+++ element-project/src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import Goods from './components/goods/Goods.vue'
+import Sellers from './components/sellers/Sellers.vue'
+import Ratings from './components/ratings/Ratings.vue'
+
+Vue.use(VueRouter);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+    linkActiveClass : 'active'
+});
+
+router.map({
+    '/goods' :{
+        component : Goods
+    },
+    '/sellers' : {
+        component: Sellers
+    },
+    '/ratings' : {
+        component: Ratings
+    }
+});
+
+router.start(app, '#app');
+
+router.go('/goods');
+
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: 'body',
+//   components: { App }
+// })
