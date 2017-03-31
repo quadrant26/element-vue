13fed3f774f4886b9d44f0b10e6db59c08a005a9
quadrant26 <quadrant26@163.com>
2017-03-31 23:45:14 +0800

    left menu and right cotent

--- element-project/src/components/goods/Goods.vue
+++ element-project/src/components/goods/Goods.vue
@@ -1,15 +1,40 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper" v-el:menu-wrapper>
+        <div class="menu-wrapper">
             <ul>
-                <li v-for="item in goods" class="item-menu" :class="{'current':currentIndex===$index}">
+                <li v-for="item in goods" class="menu-item">
                     <span class="text border-1px">
-                        <span v-show="item.type>0" class="icon" :class="classMap[item.type]">{{item.name}}</span>
+                        <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
                     </span>
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper"></div>
+        <div class="foods-wrapper">
+            <ul>
+                <li v-for="item in goods" class="food-list">
+                    <h1 class="title">{{item.name}}</h1>
+                    <ul>
+                        <li v-for="food in item.foods" class="food-item">
+                            <div class="icon">
+                                <img width="57" height="57" :src="food.icon" alt="">
+                            </div>
+                            <div class="content">
+                                <h2 class="name">{{food.name}}</h2>
+                                <p class="desc">{{food.description}}</p>
+                                <div class="extra">
+                                    <span>月售{{food.sellCount}}份</span>
+                                    <span>好评率{{food.rating}}</span>
+                                </div>
+                                <div class="price">
+                                    <span>￥{{food.price}}</span>
+                                    <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -35,7 +60,6 @@ export default {
             response = response.body
             if(response.errno == ERR_OK){
                 this.goods = response.data
-                console.log(this.goods);
             }
         }, response=> {
 
@@ -63,16 +87,10 @@ export default {
         .menu-item
             display:table
             height:54px
+            width:56px
             padding:0 12px
             line-height:14px
-            &.current
-                position: relative
-                z-index: 10
-                margin-top: -1px
-                background: #fff
-                font-weight: 700
-                .text
-                    border-none()
+
             .icon
                 display: inline-block
                 vertical-align: top
@@ -91,13 +109,42 @@ export default {
                     bg-image('invoice_3')
                 &.special
                     bg-image('special_3')
-                .text
-                    display: table-cell
-                    width: 56px
-                    vertical-align: middle
-                    border-1px(rgba(7, 17, 27, 0.1))
-                    font-size: 12px
+            .text
+                display: table-cell
+                width: 56px
+                vertical-align: middle
+                border-1px(rgba(7, 17, 27, 0.1))
+                font-size: 12px
 
     .foods-wrapper
         flex: 1
+        .title
+            padding-left: 14px
+            height: 26px
+            line-height: 26px
+            border-left:2px solid #d9dde1
+            font-size: 12px
+            color:rgb(147, 153, 159)
+            background:#f3f5f7
+        .food-item
+            display:flex
+            margin: 18px
+            padding-bottom:18px
+            border-1px(rgba(7, 17, 27, 0.1))
+            &:last-child
+                border-none()
+                margin-bottom:0
+            .icon
+                flex:0 0 57px
+                margin-right:10px
+            .content
+                flex:1
+                .name
+                    margin:2px 0 8px 0
+                    height: 14px
+                    line-height: 14px
+                    font-size:14px
+                    color:rgb(7, 17, 27)
+
+
 </style>
