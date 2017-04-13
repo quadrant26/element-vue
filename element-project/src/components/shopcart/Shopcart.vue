d54771aeb4faf2c33e8a24754f28b7e82af1293a
quadrant26 <quadrant26@163.com>
2017-04-13 22:33:34 +0800

    shopcart style and data

--- element-project/src/components/shopcart/Shopcart.vue
+++ element-project/src/components/shopcart/Shopcart.vue
@@ -3,16 +3,19 @@
         <div class="content">
             <div class="content-left">
                 <div class="logo-wrapper">
-                    <div class="logo">
-                        <span class="icon-shopping_cart"></span>
+                    <div class="logo" :class="{'highlight' : totalCount > 0}">
+                        <span class="icon-shopping_cart" :class="{'highlight' : totalCount > 0}"></span>
                     </div>
+                    <div class="num" v-show="totalCount>0">{{totalCount}}</div>
                 </div>
-                <div class="price">
-
-                </div>
-                <div class="desc"></div>
+                <div class="price" :class="{'highlight' : totalPrice > 0}">￥{{totalPrice}}</div>
+                <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
             </div>
-            <div class="content-right"></div>
+            <div class="content-right">
+                <div class="pay">
+                    ￥{{minPrice}}元
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -22,7 +25,56 @@
     export default {
         data(){
             return {
+                 
+            }
+        },
+        props : {
+            selectFoods: {
+                type : Array,
+                default() {
+                    return [
+                        {
+                            price : 5,
+                            count : 0
+                        }
+                    ];
+                }
+            },
+            deliveryPrice : {
+                type : Number,
+                default : 0
+            },
+            minPrice : {
+                type : Number,
+                default : 0
+            }
+        },
+        computed: {
+            totalPrice() {
+                let total = 0
+                this.selectFoods.forEach( (food) => {
+                    // 总价
+                    total += food.price * food.count
 
+                })
+                return total;
+            },
+            totalCount() {
+                let count = 0;
+                this.selectFoods.forEach( (food) => {
+                    count += food.count
+                });
+                return count;
+            },
+            payDesc() {
+                if(this.totalPrice === 0){
+                    return `￥{this.minPrice}元起送`;
+                }else if( this.totalPrice > this.minPrice ){
+                    let diff = this.minPrice - this.totalPrice;
+                    return `还差￥{diff}起送`;
+                }else{
+                    return `去结算`;
+                }
             }
         },
         components : {
@@ -44,6 +96,7 @@
         display:flex
         background:#131d27
         font-size:0
+        color:rgba(255, 255, 255, 0.4)
         .content-left
             flex:1
             .logo-wrapper
@@ -55,12 +108,67 @@
                 box-sizing:border-box
                 margin: 0 12px
                 padding:6px
+                vertical-align:top
+                border-radius:50%
+                background:#141d27
+                .num
+                    position:absolute
+                    top:0
+                    right:0
+                    width:24px
+                    height:16px
+                    line-height:16px
+                    text-align:center
+                    border-radius:50%
+                    font-size:9px
+                    font-weight:700
+                    background:rgb(240, 20, 20)
+                    color:#fff
+                    box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.4)
+
+                .logo
+                    width:100%
+                    height:100%
+                    border-radius:50%
+                    background:#2b343c
+                    text-align:center
+                    &.highlight
+                        background:rgb(0, 160, 220)
+                    .icon-shopping_cart
+                        color:#80858a
+                        font-size:24px
+                        line-height:44px
+                        &.highlight
+                            color:#fff
             .price
                 display:inline-block
+                font-size:16px
+                vertical-align:top
+                line-height:24px
+                margin-top:12px
+                box-sizing:border-box
+                padding-right:12px
+                border-right:1px solid rgba(255, 255, 255, 0.1)
+                font-weight:700
+                &.highlight
+                    color:#fff
             .desc
                 display:inline-block
+                vertical-align:top
+                height:24px
+                margin:12px 0 0 12px
+                line-height:24px
+                font-size:10px
+
         .content-right
             flex:0 0 105px
-
+            width:105px
+            .pay
+                height:48px
+                line-height:48px
+                text-align:center
+                font-size:12px
+                font-weight:700
+                background: #2b333b
 
 </style>
