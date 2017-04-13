a21cc6519e62d7d9ceb42f988cfca65590c1c44f
quadrant26 <quadrant26@163.com>
2017-04-13 23:52:01 +0800

    transition move

--- element-project/src/components/cartcontrol/Cartcontrol.vue
+++ element-project/src/components/cartcontrol/Cartcontrol.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="cartcontrol">
-        <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart" transition="move">
-            <span class="inner"></span>
+        <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart" transition="move">
+            <span class="inner icon-remove_circle_outline"></span>
         </div>
         <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
         <div class="cart-add icon-add_circle" @click="addCart"></div>
@@ -53,22 +53,38 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
     .cartcontrol
         font-size:0
-        .cart-decrease, .cart-add
+        .cart-decrease
+            display:inline-block
+            padding:6px
+            transition:all 0.4s linear
+            &.move-transition
+                opacity:1
+                transform:translate3d(0, 0, 0)
+            .inner
+                display:inline-block
+                line-height:24px
+                font-size:24px
+                color:rgb(0, 160, 220)
+                transition: all 0.4s linear
+                transform: rotate(0)
+            &.move-enter, &.move-leave
+                opacity:0
+                transform:translate3d(24px, 0, 0)
+                .inner
+                    transform:rotate(180deg)
+        .cart-count
+            display:inline-block
+            vertical-align:top
+            width:12px
+            font-size:10px
+            text-align:center
+            line-height:24px
+            padding-top:6px;
+            color:rgb(147, 153, 159)
+        .cart-add
             display:inline-block
             padding:6px
             line-height:24px
             font-size:24px
             color:rgb(0, 160, 220)
-        .cart-decrease
-            display:inline-block
-        .cart-count
-            display:inline-block
-            width:12px
-            font-size:10px
-            line-height:24px
-            vertical-align:top
-            padding-top:6px;
-            color:rgb(147, 153, 159)
-        .cart-add
-            display:inline-block
 </style>
